40a9f6fc1082c281da830c98498a884c3bc9a5ef
55tommy <tomisawakana@gmail.com>
2022-12-22 11:07:45 +0900

    予約一覧のview設定

--- app/controllers/appointments_controller.rb
+++ app/controllers/appointments_controller.rb
@@ -1,5 +1,13 @@
 class AppointmentsController < ApplicationController
   def index
-    @appointments = Appointement.all
+    @appointments = Appointment.all.where("day >= ?", Date.current).where("day < ?", Date.current >> 3).order(day: :desc)
+  end
+
+  def new
+    @appointment = Appointment.new
+    @day = params[:day]
+    @time = params[:time]
+    @start_time = DateTime.parse
   end
 end
+
